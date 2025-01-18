c1718dba9bfb8656c931f2734b8339db50d09bf6
Eravity <cebotaricristian92@gmail.com>
2025-01-18 18:55:35 +0200

    Add logo image

--- src/app/_components/Profile.tsx
+++ src/app/_components/Profile.tsx
@@ -2,15 +2,17 @@ import ArrowDown from "./ArrowDown";
 
 export default function Profile() {
   return (
-    <div className="flex space-x-2">
-      <div className="w-12 h-12 flex justify-center items-center bg-skySplash rounded-lg">
+    <div className="flex space-x-3 items-center">
+      <div className="w-10 h-10 flex justify-center items-center bg-skySplash rounded-lg">
         <h1 className="text-white font-bold">CC</h1>
       </div>
+
       <div>
         <h1 className="font-bold">Cristian Cebotari</h1>
         <p className="text-neutral-500">FullStack Developer</p>
       </div>
-      <ArrowDown/>
+
+      <ArrowDown />
     </div>
   );
 }
