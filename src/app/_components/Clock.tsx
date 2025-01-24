4a3d18bd0a8fb13c8ff0678fc88d0cd3384ec845
Eravity <cebotaricristian92@gmail.com>
2025-01-24 14:42:30 +0200

    Change color of deadline based on remaining time

--- src/app/_components/Clock.tsx
+++ src/app/_components/Clock.tsx
@@ -1,20 +1,20 @@
-export default function Clock() {
+export default function Clock({color}: {color?: string}) {
   return (
     <div>
       <svg
-        width="25px"
-        height="25px"
+        width="20px"
+        height="20px"
         viewBox="0 0 24 24"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
       >
         <path
           d="M12 7V12L14.5 10.5M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
-          stroke="#ffffff"
+          stroke={color || "#737373"}
           strokeWidth="2"
           strokeLinecap="round"
           strokeLinejoin="round"
-          fill="#FFAC00"
+          fill="none"
         />
       </svg>
     </div>
--- src/app/_components/CourseProgress.tsx
+++ src/app/_components/CourseProgress.tsx
@@ -1,7 +1,9 @@
 import Image from "next/image";
+import Link from "next/link";
 import image1 from "../images/1.jpg";
 import image2 from "../images/2.jpg";
 import { StaticImageData } from "next/image";
+import Clock from "./Clock";
 
 type CourseData = {
   image: StaticImageData;
@@ -10,7 +12,7 @@ type CourseData = {
   competition: number;
   deadline: number;
   buttonText: string;
-}
+};
 
 const courses: CourseData[] = [
   {
@@ -19,24 +21,39 @@ const courses: CourseData[] = [
     materials: 5,
     competition: 0,
     deadline: 1,
-    buttonText: "Start"
+    buttonText: "Start",
   },
   {
     image: image2,
     title: "Creating Engaging Learning Jour...",
     materials: 14,
     competition: 64,
-    deadline: 12,
-    buttonText: "Continue"
-  }
+    deadline: 24,
+    buttonText: "Continue",
+  },
 ];
 
-const InfoCell = ({ label, value }: { label: string; value: string | number }) => (
+const InfoCell = ({
+  label,
+  value,
+  icon,
+  isUrgent,
+}: {
+  label: string;
+  value: string | number;
+  icon?: React.ReactNode;
+  isUrgent?: boolean;
+}) => (
   <div className="flex items-center justify-center flex-col p-2 space-y-1">
     <h2 className="w-full h-1/2 flex items-center text-neutral-700 font-semibold text-sm">
       {label}
     </h2>
-    <h2 className="w-full h-1/2 flex items-center font-semibold">
+    <h2
+      className={`w-full h-1/2 flex gap-1 items-center font-semibold ${
+        isUrgent ? "text-red-500" : ""
+      }`}
+    >
+      {icon}
       {value}
     </h2>
   </div>
@@ -46,25 +63,36 @@ const CourseRow = ({ course }: { course: CourseData }) => (
   <>
     <div className="flex items-center justify-center p-2">
       <div className="relative w-full h-full">
-        <Image
-          src={course.image}
-          alt={course.title}
-          fill
-          className="object-cover rounded-lg absolute inset-0"
-        />
+        <Link href={''}>
+          <Image
+            src={course.image}
+            alt={course.title}
+            fill
+            className="object-cover rounded-lg absolute inset-0"
+          />
+        </Link>
       </div>
     </div>
     <div className="col-span-3 flex flex-col space-y-1 items-center justify-center p-2">
       <h2 className="w-full h-1/2 flex items-center text-neutral-700 font-semibold text-sm">
         Course
       </h2>
-      <h2 className="w-full h-1/2 text-xl font-bold">
+      <Link href={""} className="w-full h-1/2 text-xl font-bold">
         {course.title}
-      </h2>
+      </Link>
     </div>
     <InfoCell label="Content" value={`${course.materials} Materials`} />
     <InfoCell label="Competition" value={`${course.competition}%`} />
-    <InfoCell label="Deadline" value={`${course.deadline} hrs`} />
+    <InfoCell
+      icon={<Clock color={course.deadline < 24 ? "red" : "#737373"} />}
+      label="Deadline"
+      value={
+        course.deadline >= 24
+          ? `${Math.floor(course.deadline / 24)}D ${course.deadline % 24}h`
+          : `${course.deadline}h`
+      }
+      isUrgent={course.deadline < 24}
+    />
     <div className="flex items-center justify-end pr-7">
       <button className="px-4 py-1 border rounded-md">
         {course.buttonText}
