33a37507720234649261b5f13cf8444a010309f5
Eravity <cebotaricristian92@gmail.com>
2025-02-20 22:22:18 +0200

    Refactor CurrentPath component

--- src/app/_components/CurrentPath.tsx
+++ src/app/_components/CurrentPath.tsx
@@ -1,12 +1,19 @@
 "use client";
 import { useEffect, useState } from "react";
+import Link from "next/link";
 import getCurrentPath from "@/app/utils/getCurrentPath";
 import { getCourse } from "@/app/supabase/data-service";
-import useCapitalize from "@/app/hooks/useCapitalize";
+
+// Helper function to capitalize strings
+function capitalize(title: string): string {
+  return title.charAt(0).toUpperCase() + title.slice(1);
+}
 
 export default function CurrentPath({ id }: { id: number }) {
   const [courseTitle, setCourseTitle] = useState<string>("");
   const currentPath = getCurrentPath();
+  const pathSegments = currentPath.split("/").filter(Boolean);
+  const breadcrumbSegments = pathSegments.filter(segment => isNaN(Number(segment)));
 
   useEffect(() => {
     async function fetchCourse() {
@@ -18,12 +25,24 @@ export default function CurrentPath({ id }: { id: number }) {
 
   return (
     <h1 className="font-semibold">
-      <span className="text-neutral-500">
-        {useCapitalize({ title: currentPath.split("/")[1] })} /{" "}
-      </span>
-      <span className="text-black">
-        {useCapitalize({ title: courseTitle })}
-      </span>
+      {breadcrumbSegments.map((segment, index) => {
+        // For the first element, navigate to the main page ("/")
+        // For other elements, navigate to the corresponding location in the path
+        const subPath = index === 0 ? "/" : "/" + breadcrumbSegments.slice(0, index + 1).join("/");
+        return (
+          <Link key={index} href={subPath}>
+            <span className="cursor-pointer">
+              {capitalize(segment)}
+              {index < breadcrumbSegments.length - 1 && " / "}
+            </span>
+          </Link>
+        );
+      })}
+      {courseTitle && (
+        <span className="text-black">
+          {" / "}{capitalize(courseTitle)}
+        </span>
+      )}
     </h1>
   );
 }
--- src/app/hooks/useCapitalize.ts
+++
@@ -1,8 +0,0 @@
-export default function useCapitalize({ title }: { title: string }): string {
-  return title
-    .split(" ")
-    .map((word) => {
-      return word.charAt(0).toUpperCase() + word.slice(1);
-    })
-    .join(" ");
-}
