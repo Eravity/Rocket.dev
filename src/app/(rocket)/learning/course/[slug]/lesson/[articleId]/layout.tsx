c19d59b6fbe270a8066421732d5b8a23b310e227
Eravity <cebotaricristian92@gmail.com>
2025-04-03 00:02:29 +0300

    Refactor lesson layout to improve structure and styling

--- src/app/(rocket)/learning/course/[slug]/lesson/[articleId]/layout.tsx
+++ src/app/(rocket)/learning/course/[slug]/lesson/[articleId]/layout.tsx
@@ -2,13 +2,15 @@ import ChapterContent from "@/app/_components/ChapterContent";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-[calc(100vh-117px)] overflow-hidden">
-      <main className="w-full overflow-y-auto flex justify-end h-full">
-        <div className="min-h-[200vh] w-4/5 bg-slate-400">
+    <div className="relative w-full h-[calc(100vh-117px)] overflow-hidden">
+      <main className="overflow-y-auto h-full flex justify-center pr-[200px]">
+        <div className="min-h-[200vh] bg-slate-400 max-w-[1200px] w-[100%]">
           {children}
         </div>
       </main>
-      <ChapterContent className="h-full w-1/4 max-w-[500px] bg-neutral-200 flex-shrink-0 overflow-y-auto"/>
+      <aside className="absolute top-0 right-0 w-1/4 max-w-[500px] h-full overflow-y-auto">
+        <ChapterContent className="w-full h-full bg-neutral-200" />
+      </aside>
     </div>
   );
 }
